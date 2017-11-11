49a6b1aa46fab3b1cd92fc45614ddb55b5b83208
”FaisalAhmed29” <FaisalAhmed29@gmail.com>
2017-11-12 01:33:29 +0600

    Necessary Files

---
+++ Necessary files/form.js
@@ -0,0 +1,14 @@
+<html>
+<body>
+  <form method="get" name="myForm" action="submit.php">
+    Salaried: <input type="checkbox" name="salaried"/>
+    <br/>
+    First Name: <input type="text" name="input1"/>
+    <br/>
+    Last Name: <input type="text" name="input2"/>
+    <br/>
+    <input type="button" value="Submit" onclick="submitForm()"/>
+  </form>
+  <script type="text/javascript" src="form_submit_script.js"></script>
+</body>
+</html>
